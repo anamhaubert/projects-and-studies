3fd711f7958597e6e2f3e6718bc63878abb1f6e3
Ana Haubert <146394925+anamhaubert@users.noreply.github.com>
2023-11-06 20:09:58 -0300

    Add files via upload

---
+++ PHP/CRUD/util/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//Configurar essas variáveis de acordo com o seu ambiente
+define("DB_HOST", "localhost");
+define("DB_NAME", "crud_dev2");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
